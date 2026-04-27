132d43bc4cf018592db8f6275406cf17d3bf96a3
xagan <amirxagan@gmal.com>
2026-04-27 22:58:26 +0330

    vercel edits

--- app/routes/projects.smart-sparrow/route.js
+++ app/routes/projects.smart-sparrow/route.js
@@ -2,4 +2,6 @@ export const loader = () => {
   throw new Response('Not Found', { status: 404 });
 };
 
-export { SmartSparrow as default, meta } from './smart-sparrow';
+export default function SmartSparrowRoute() {
+  return null;
+}
